19270c6c88bb7cba3c2080ac73c331473d8b12eb
hkadakia05 <144747086+hkadakia05@users.noreply.github.com>
2026-07-16 22:35:25 -0700

    Push change dialy july 16th

--- TwoSum-May10/Twosumsolution.py
+++ TwoSum-May10/Twosumsolution.py
@@ -1,1 +1,18 @@
 #TwoSumSolution in Python w/ comments
+
+#Give below solve for solution
+#class Solution:
+   # def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+
+
+#Inital Brutforce Solution
+class Solution: #given
+    def twoSum(self, nums: List[int], target: int) -> List[int]: #given
+        for i in range(len(nums)): #checks the indicies i is in the array nums 
+        #example of the i could be indicie 0 in the array [7,94,204,23]
+        #where i = 0 and the 
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
