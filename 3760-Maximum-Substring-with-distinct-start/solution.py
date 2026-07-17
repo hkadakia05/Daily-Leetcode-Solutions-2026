aadec0352f72569e2245dac9121241e7eab81355
hkadakia05 <144747086+hkadakia05@users.noreply.github.com>
2026-07-16 22:37:47 -0700

    text context

--- 3760-Maximum-Substring-with-distinct-start/solution.py
+++ 3760-Maximum-Substring-with-distinct-start/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def maxDistinct(self, s: str) -> int:
         return len(set(s))
-        
+        #The solution is simple return the set of s because a set is only continiing the unique characters whoich is all you need for the function and then len just returns the unique characters amount whch is what the function and solution is asking for.
